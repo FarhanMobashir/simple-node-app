83c5333147b01c571c47a4f8c96e6db4feaf0ec4
FarhanMobashir <farhanmobashir007@gmail.com>
2024-07-05 23:28:41 +0530

    from push event

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World From Testing');
+    res.send('Hello World From Push Event');
 });
 
 if (require.main === module) {
