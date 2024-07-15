56409fc12fcacd9695115540d5c1064ab6ce78fb
FarhanMobashir <farhanmobashir007@gmail.com>
2024-07-15 13:39:37 +0530

    fixed the test

--- app.test.js
+++ app.test.js
@@ -7,13 +7,13 @@ describe('GET /', () => {
     it('should respond with Hello World!', async () => {
         const response = await request(app).get('/');
         expect(response.status).toBe(200);
-        expect(response.text).toBe('Hello World from Nvim!');
+        expect(response.text).toBe('Hello World From Remote');
     });
 });
 
 describe('GET /abc', () => {
     it('should respond with a 404 status for the dummy route', async () => {
         const response = await request(app).get('/abc');
-        expect(response.status).toBe(403);
+        expect(response.status).toBe(404);
     });
 });
