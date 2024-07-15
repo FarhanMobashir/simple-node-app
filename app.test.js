513cf0a38d48b9a6f9de5003e19c5a6f0e74753f
FarhanMobashir <farhanmobashir007@gmail.com>
2024-07-15 12:56:23 +0530

    another push

--- app.test.js
+++ app.test.js
@@ -1,13 +1,13 @@
 const request = require('supertest');
 const express = require('express');
 
-const app = require('./index'); // Assuming your app is in app.js
+const app = require('./index'); // importing the app from
 
 describe('GET /', () => {
     it('should respond with Hello World!', async () => {
         const response = await request(app).get('/');
         expect(response.status).toBe(200);
-        expect(response.text).toBe('Hello World!');
+        expect(response.text).toBe('Hello World from Nvim!');
     });
 });
 
